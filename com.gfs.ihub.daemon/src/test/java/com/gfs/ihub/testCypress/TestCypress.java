3d0a56b6a4afb88789b337957fb7e6ff31c0884a
Scott Rosenbaum <scottr@innoventsolutions.com>
2014-10-09 19:27:17 -0500

    Add Tests, Move Test src folder to appropriate loc

---
+++ com.gfs.ihub.daemon/src/test/java/com/gfs/ihub/testCypress/TestCypress.java
@@ -0,0 +1,14 @@
+package com.gfs.ihub.testCypress;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCypress {
+
+	@Test
+	public void testSuccess() {
+		assertTrue(true);
+	}
+
+}
---
+++ com.gfs.ihub.daemon/src/test/java/com/gfs/ihub/testEmail/TestEmail.java
@@ -0,0 +1,14 @@
+package com.gfs.ihub.testEmail;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestEmail {
+
+	@Test
+	public void testSuccess() {
+		assertTrue(true);
+	}
+
+}
